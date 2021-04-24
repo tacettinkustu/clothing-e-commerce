b073fdd202a4083ac460cad2d252204c0b3aa4a5
tacettinkustu <tacettinkustu>
2021-04-24 14:40:09 +0300

    firebase firestore

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import "./custom-button-styles.scss"
 
-const CustomBotton = ({children,...otherProps}) => {
+const CustomBotton = ({children,isGoogleSignIn,...otherProps}) => {
     return (
-        <button className="custom-button" {...otherProps}>
+        <button className={`${isGoogleSignIn?"google-sign-in":""} custom-button`} {...otherProps}>
         {children}
         </button>
     )
