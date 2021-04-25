403725116ea2ec39a9c16ec0da554d5f2b7e37a3
tacettinkustu <tacettinkustu>
2021-04-25 17:27:45 +0300

    cart-items

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import "./custom-button-styles.scss"
+import React from "react";
+import "./custom-button-styles.scss";
 
-const CustomBotton = ({children,isGoogleSignIn,...otherProps}) => {
-    return (
-        <button className={`${isGoogleSignIn?"google-sign-in":""} custom-button`} {...otherProps}>
-        {children}
-        </button>
-    )
-}
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => (
+  <button
+    className={`${inverted ? "inverted" : ""} ${
+      isGoogleSignIn ? "google-sign-in" : ""
+    } custom-button`}
+    {...otherProps}
+  >
+    {children}
+  </button>
+);
 
-export default CustomBotton
+export default CustomButton;
